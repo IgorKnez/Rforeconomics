4874ca16ca7c41b23d7cade74650092d30c278f9
IgorKnez <iknez11@gmail.com>
2018-10-03 17:39:08 +0200

    exercise 2 r for econometrics

---
+++ exercise2.R
@@ -0,0 +1,38 @@
+
+# print system time
+
+Sys.time()
+
+# create bad list
+
+bad_list <- list(na = c(NA, NA, NA), chars = c('a', 'b', 'c'), 
+                 bools = c(FALSE, FALSE, TRUE))
+
+# create data frame happy frame:
+
+happy_frame <- data.frame(na = c(NA, NA, NA), chars = c('a', 'b', 'c'), 
+                          bools = c(FALSE, FALSE, TRUE))
+
+x <- 1:16
+
+
+# write the length function
+
+sum(x) / mean(x)
+
+# 3rd position is TRUE
+
+x %% 3 == 0
+
+
+#red fruit larger than 5
+
+fruit_size = c(2, 4, 7, 5, 3, 2)
+fruit_col = c('g', 'y', 'r', 'g', 'b', 'r')
+
+
+(fruit_col == 'r' & fruit_size > 5) | (fruit_col != 'r' & fruit_size < 5)
+
+
+
+
